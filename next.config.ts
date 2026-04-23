2efbe9da4788c444da5f3fdafbb66ce257fec66d
LKEEN <gvxioffical@gmail.com>
2026-04-23 15:30:33 +0400

    fix: remove edgeExternals configuration and ensure proper newline at end of file 3

--- next.config.ts
+++ next.config.ts
@@ -4,17 +4,17 @@ import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 initOpenNextCloudflareForDev();
 
 const nextConfig: NextConfig = {
+    outputFileTracingExcludes: {
+	"*": [
+        	"./node_modules/@react-pdf/**/*",
+			"./node_modules/fontkit/**/*",
+			"./node_modules/brotli/**/*",
+		],
+	},
 	turbopack: {
 		root: process.cwd(),
 	},
 	experimental: {
-		outputFileTracingExcludes: {
-			"*": [
-				"./node_modules/@react-pdf/**/*",
-				"./node_modules/fontkit/**/*",
-				"./node_modules/brotli/**/*",
-			],
-		},
 	},
 };
 
