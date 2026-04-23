434dff21c25d543f3d0a6fd477cf92147a6d7b24
LKEEN <gvxioffical@gmail.com>
2026-04-23 16:05:43 +0400

    feat: implement proxy middleware for request handling and authentication

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,13 @@
 import type { NextConfig } from "next";
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
-
-initOpenNextCloudflareForDev();
 
 const nextConfig: NextConfig = {
-    outputFileTracingExcludes: {
-	"*": [
-        	"./node_modules/@react-pdf/**/*",
-			"./node_modules/fontkit/**/*",
-			"./node_modules/brotli/**/*",
-		],
-	},
-	turbopack: {
-		root: process.cwd(),
-	},
-	experimental: {
-	},
+  outputFileTracingExcludes: {
+    "*": [
+      "./node_modules/@react-pdf/**/*",
+      "./node_modules/fontkit/**/*",
+      "./node_modules/brotli/**/*",
+    ],
+  },
 };
 
 export default nextConfig;
